ae74e4af4e8a207d0accd69a8f4eb63d478349fb
obafisayo-alx <sekaniisjoy@gmail.com>
2023-11-28 03:48:15 +0100

    betty

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,17 +9,17 @@
 */
 int check_cycle(listint_t *list)
 {
-     listint_t *slow = list, *fast = list;
+	listint_t *slow = list, *fast = list;
 
-     if (list == NULL)
-          return (0);
-     while (fast != NULL && fast->next != NULL)
-     {
-          slow = slow->next;
-          fast = fast->next->next;
-          if (slow == fast)
-               return (1);
-          
-     }
-     return (0);
+	if (list == NULL)
+		return (0);
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+		
+	}
+	return (0);
 }
