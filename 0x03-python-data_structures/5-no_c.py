c33a4f3993e1aa655a92d3e80b2bea88c65eca06
obafisayo-alx <sekaniisjoy@gmail.com>
2023-12-05 00:13:02 +0100

    added task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def no_c(my_string):
+	result = ""
+	for i in my_string:
+		if i.lower() != "c":
+			result += i
+	return result
+
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
