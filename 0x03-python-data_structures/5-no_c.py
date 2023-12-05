54ec310132c2561536669ccbca770a4aff3837e9
obafisayo-alx <sekaniisjoy@gmail.com>
2023-12-05 22:30:12 +0100

    done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-
 def no_c(my_string):
-    if (my_string):
-        new_string = ""
-        for i in my_string:
-            if i == "c" or i == "C":
-                continue
-            else:
-                new_string += i
-    return (new_string)
+    return(''.join(x for x in my_string if x not in 'Cc'))
