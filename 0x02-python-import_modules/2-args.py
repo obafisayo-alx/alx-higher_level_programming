0cf1ec90ff313f8dde65aa19c280d53e3e0e0e05
obafisayo-alx <sekaniisjoy@gmail.com>
2023-12-05 22:42:48 +0100

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv)
-    if(args <= 1):
+    if (args <= 1):
         print("{} arguments.".format(args - 1))
-    elif(args == 2):
+    elif (args == 2):
         print("{} argument:".format(args - 1))
     else:
         print("{} arguments:".format(args - 1))
