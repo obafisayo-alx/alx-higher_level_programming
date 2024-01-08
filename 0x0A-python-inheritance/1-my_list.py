d1a170bbb7e73ae4b99931feab0ad303ee4421e7
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-09 00:04:56 +0100

    added task 1 and a test file for my_list.txt

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This shows a class MyList that inherits from the class list"""
+
+
+class MyList(list):
+    """This prints sorted list of class"""
+    def print_sorted(self):
+        print(sorted(list))
