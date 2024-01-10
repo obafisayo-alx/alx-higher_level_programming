a964232c2345283a928d3c980bfad57662da4101
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 22:05:11 +0100

    hgjhc

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
-"""Add all arguments to a list and save its JSON encoding to a file.
+"""Add all arguments to a Python list and save them to a JSON file."""
 
-This script takes command-line arguments, loads existing data from a JSON file
-if available, adds the command-line arguments to the existing data, and saves
-the updated data back to the file in JSON format.
-
-Usage:
-    ./script_name.py arg1 arg2 ...
-
-    Example:
-    ./script_name.py apple banana orange
-
-"""
 
 from sys import argv
+from json import load, dump
 
-# Importing functions from external modules
-save_to_json = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json = __import__('6-load_from_json_file.py').load_from_json_file
 
-# File name for storing the data in JSON format
-FILENAME = 'add_item.json'
+def save_to_json_file(my_obj, filename):
+    """Save object to a JSON file."""
+    with open(filename, 'w', encoding='utf-8') as file:
+        dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """Load object from a JSON file."""
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            return load(file)
+    except FileNotFoundError:
+        return []
+
 
 if __name__ == '__main__':
-    try:
-        # Try to load existing data from the JSON file
-        existing_data = load_from_json(FILENAME)
+    # Load existing data from add_item.json
+    existing_data = load_from_json_file('add_item.json')
 
-        # Add command-line arguments to the existing data
-        updated_data = existing_data + argv[1:]
+    # Add command-line arguments to the existing data
+    updated_data = existing_data + argv[1:]
 
-        # Save the updated data to the JSON file
-        save_to_json(updated_data, FILENAME)
-    except (FileNotFoundError, ValueError):
-        # If file not found or invalid JSON data, create a new file with
-        # only the command-line arguments
-        save_to_json(argv[1:], FILENAME)
+    # Save the updated data to add_item.json
+    save_to_json_file(updated_data, 'add_item.json')
