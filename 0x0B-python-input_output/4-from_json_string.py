e5dad80c2d9fcdcfe31380fcba5117eb47e69f2d
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 01:53:01 +0100

    added 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""object represented as json string"""
+import json
+
+def from_json_string(my_str):
+     """convert json string to object
+
+     Args:
+          my_str (str): json string to be deserialized
+     
+     Returns: object represented by `my_str`
+     """
+     return json.loads(my_str)
