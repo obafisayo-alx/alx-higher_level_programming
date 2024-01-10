d11121c781e5355488525cc717bb4b621d5c4494
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 23:44:15 +0100

    pascal fucking triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""pascals Triangle"""
+
+
+def pascal_triangle(n):
+    """creates a pascal triangle with n rows"""
+    triangle = []
+    prow = []
+    for i in range(n):
+        crow = [1] + [sum(prow[el:el + 2]) for el in range(len(prow - 1))]
+        if i > 0:
+            crow = crow + [1]
+        triangle.append(crow)
+        prow = crow
+    return triangle
