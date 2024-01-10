f210798f412d63fd5ef252904e58de0cf7c0214c
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 23:48:13 +0100

    pascal is annoying

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,7 +7,7 @@ def pascal_triangle(n):
     triangle = []
     prow = []
     for i in range(n):
-        crow = [1] + [sum(prow[el:el + 2]) for el in range(len(prow - 1))]
+        crow = [1] + [sum(prow[el:el + 2]) for el in range(len(prow) - 1)]
         if i > 0:
             crow = crow + [1]
         triangle.append(crow)
