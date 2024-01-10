bfd639edb8ad7110a5f8e118171a4526151aa171
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 21:47:01 +0100

    all files to 11th file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module to implement class and method to serialize itself"""
+
+
+class Student:
+    """A class to rep student and methods to create json strings"""
+    def __init__(self, first_name, last_name, age):
+        """initialize student instance with first, last names and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """creates copy of attributes for use in json string"""
+        if attrs is None or not isinstance(attrs, (list, tuple)):
+            return self.__dict__.copy()
+        else:
+            res = {k: v for k, v in filter(lambda x: x[0] in attrs,
+                                           self.__dict__.items())}
+            return res
+
+    def reload_from_json(self, json):
+        if not isinstance(json, dict):
+            raise ValueError("Input must be a dictionary")
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+    # alternatively,
+    # self.__dict__.update(json) - this does the same thing as the code above
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Add all arguments to a list and save its JSON encoding to a file"""
 
+
 from sys import argv
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json = __import__('6-load_from_json_file.py').load_from_json_file
 
 FILENAME = 'add_item.json'
 
 if __name__ == '__main__':
     try:
-        save_to_json_file(load_from_json_file(FILENAME) + argv[1:], FILENAME)
+        save_to_json(load_from_json(FILENAME) + argv[1:], FILENAME)
     except (FileNotFoundError, ValueError):
-        save_to_json_file(argv[1:], FILENAME)
+        save_to_json(argv[1:], FILENAME)
