004d2416933cfca4fa71f17532e9f1f6d5f2fbcf
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 01:08:14 +0100

    debugged task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,11 @@
 
 
 def read_file(filename=""):
-     """opens a file for read only"""
-     with open(filename, "r", encoding="utf-8") as myfile:
-          read_data = myfile.read()
-          print(read_data)
+    """opens a file for read only
+     
+    Args:
+        filename (str): file to open
+    """
+    with open(filename, 'r', encoding='utf-8') as myfile:
+        read_data = myfile.read()
+        print(read_data, end="")
