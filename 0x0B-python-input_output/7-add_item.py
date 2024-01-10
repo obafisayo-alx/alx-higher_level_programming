23ac9795d7f621b032554d79519cd410ec35275f
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 22:07:38 +0100

    hope it works

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,22 +3,10 @@
 
 
 from sys import argv
-from json import load, dump
 
 
-def save_to_json_file(my_obj, filename):
-    """Save object to a JSON file."""
-    with open(filename, 'w', encoding='utf-8') as file:
-        dump(my_obj, file)
-
-
-def load_from_json_file(filename):
-    """Load object from a JSON file."""
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            return load(file)
-    except FileNotFoundError:
-        return []
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == '__main__':
