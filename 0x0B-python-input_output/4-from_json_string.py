dc12e1a2894d7e20c02b1789e7d137eefe0bb5d4
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 09:31:13 +0100

    debugged ps

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,12 +2,13 @@
 """object represented as json string"""
 import json
 
-def from_json_string(my_str):
-     """convert json string to object
 
-     Args:
-          my_str (str): json string to be deserialized
-     
-     Returns: object represented by `my_str`
-     """
-     return json.loads(my_str)
+def from_json_string(my_str):
+    """convert json string to object
+
+    Args:
+        my_str (str): json string to be deserialized
+
+    Returns: object represented by `my_str`
+    """
+    return json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,7 +9,7 @@ def save_to_json_file(my_obj, filename):
     Args:
         my_obj (obj): object file to be serialzed
         filename (str): file to store json representation
-    
+
     Returns: saves json representation to file
     """
     with open(filename, 'w', encoding='utf8') as f:
