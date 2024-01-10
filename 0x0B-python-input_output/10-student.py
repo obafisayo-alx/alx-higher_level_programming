94643cbd0596981d1b18635900d92e9b530424af
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 16:24:05 +0100

    add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to implement class and method to serialize itself"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None or not isinstance(attrs, (list, tuple)):
+            return self.__dict__.copy()
+        else:
+            res = {k: v for k, v in filter(lambda x: x[0] in attrs,
+                                            self.__dict__.items())}
+            return res
