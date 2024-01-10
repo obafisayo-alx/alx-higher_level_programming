b174205908bea65183af78685ae39b6bc898e7f3
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 09:18:14 +0100

    added task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Object to text file using json"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """convert an object `my_obj` to json and save to `filename`
+
+    Args:
+        my_obj (obj): object file to be serialzed
+        filename (str): file to store json representation
+    
+    Returns: saves json representation to file
+    """
+    with open(filename, 'w', encoding='utf8') as f:
+        f.write(json.dumps(my_obj))
