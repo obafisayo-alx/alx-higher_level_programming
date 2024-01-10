a9707e7787e88bebcbe8b9d2bf88208e03e6da6d
obafisayo-alx <sekaniisjoy@gmail.com>
2024-01-10 22:11:34 +0100

    HMM

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,12 +9,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
+FILENAME = "add_item.json"
+
+
 if __name__ == '__main__':
-    # Load existing data from add_item.json
-    existing_data = load_from_json_file('add_item.json')
-
-    # Add command-line arguments to the existing data
-    updated_data = existing_data + argv[1:]
-
-    # Save the updated data to add_item.json
-    save_to_json_file(updated_data, 'add_item.json')
+    try:
+        save_to_json_file(load_from_json_file(FILENAME) + argv[1:], FILENAME)
+    except (FileNotFoundError, ValueError):
+        save_to_json_file(argv[1:], FILENAME)
