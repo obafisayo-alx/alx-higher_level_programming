456de6c4613dbc4b66c418353d9c635bc8e34742
obafisayo-alx <sekaniisjoy@gmail.com>
2024-02-15 05:16:47 +0100

    ok

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Grant necessary privileges to user_0d_1
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant necessary privileges to user_0d_2
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
