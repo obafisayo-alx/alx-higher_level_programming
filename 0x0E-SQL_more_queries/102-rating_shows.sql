20c47f1f0303eec545a106da301f05906425a34d
obafisayo-alx <sekaniisjoy@gmail.com>
2024-02-15 05:46:57 +0100

    omo wahal dey e choke

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,5 @@
+SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+GROUP BY tv_shows.title
+ORDER BY rating_sum DESC;
---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+SELECT tv_genres.name, SUM(tv_show_ratings.rating) AS rating_sum
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+GROUP BY tv_genres.name
+ORDER BY rating_sum DESC;
