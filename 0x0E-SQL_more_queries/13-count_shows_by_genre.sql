118a08db4f46f176894b635eb0ecc8cd473207f0
obafisayo-alx <sekaniisjoy@gmail.com>
2024-02-15 05:39:42 +0100

    jupcatd e ahbd dey r polay for me ther e

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+SELECT genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name
+ORDER BY number_of_shows DESC;
