683705c5cfe62c134420799680b0f82f5b3da2f1
obafisayo-alx <sekaniisjoy@gmail.com>
2024-03-12 07:21:14 +0100

    added task 3 and updated the shebang in the previous files

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,7 +5,5 @@ const argsCount = process.argv.length;
 if (argsCount === 2) {
   console.log('No argument');
 } else if (argsCount === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
+  console.log(process.argv[2]);
 }
