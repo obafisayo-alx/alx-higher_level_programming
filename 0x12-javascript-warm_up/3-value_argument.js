5c39d284592e6af6a6fd584ce13bae943d8e5280
obafisayo-alx <sekaniisjoy@gmail.com>
2024-03-13 02:16:23 +0100

    debuged task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 
-const argsCount = process.argv.length;
-
-if (argsCount === 2) {
-  console.log('No argument');
-} else if (argsCount === 3) {
-  console.log(process.argv[2]);
-}
+console.log(process.argv[2]);
