35987f83829720bc5e058c09500934df466e226a
obafisayo-alx <sekaniisjoy@gmail.com>
2024-03-13 03:03:02 +0100

    added task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < strArray.length; index++) {
+  const element = strArray[index];
+  console.log(element);
+}
