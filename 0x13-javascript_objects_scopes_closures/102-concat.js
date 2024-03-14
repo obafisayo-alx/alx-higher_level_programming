4ca5887c7455d05373f48a63d90825d67a203219
obafisayo-alx <sekaniisjoy@gmail.com>
2024-03-14 06:51:04 +0100

    again

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -10,14 +10,12 @@ if (process.argv.length !== 5) {
 // Get file paths from command line arguments
 const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
 
-// Read the contents of the source files
 const content1 = fs.readFileSync(sourceFile1, 'utf8');
 const content2 = fs.readFileSync(sourceFile2, 'utf8');
 
-// Concatenate the contents
-const concatenatedContent = content1 + '\n' + content2;
+const concatenatedContent = content1 + content2;
 
 // Write the concatenated content to the destination file
 fs.writeFileSync(destinationFile, concatenatedContent);
 
-console.log(`Files ${sourceFile1} and ${sourceFile2} concatenated to ${destinationFile}.`);
+// console.log(`Files ${sourceFile1} and ${sourceFile2} concatenated to ${destinationFile}.`);
