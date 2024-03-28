d1dd47e7e3e4571faf032903972cf77be9794a43
obafisayo-alx <sekaniisjoy@gmail.com>
2024-03-29 00:07:55 +0100

    pycodestyle debugged

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,8 @@ import urllib.parse
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type:', type(html))
