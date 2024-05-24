0a1011fac5b662fab99c2cbca8c36f30a1346f59
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-24 21:11:35 +0100

    modified

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-const file_path = process.argv[2];
-
-import fs from "fs"
-fs.readFile(file_path, 'utf-8', (err, data) => {
-  if(err) {
-    console.error(err);
-  } else {
-    console.log(data)
-  }
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
